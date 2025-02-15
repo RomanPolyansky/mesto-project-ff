54f08628ddae53978cfd26b0d33490eaae019f94
Roman <roman_polyan@mail.ru>
2025-02-15 13:25:20 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,6 @@
 const cardsList = getCardsList();
 renderCards();
 
-function createCard(card) {
-  const cardTemplate = getCardTemplate();
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-
-  const cardImage = cardElement.querySelector('.card__image');
-  cardImage.src = card.link;
-  cardImage.alt = card.name;
-
-  cardElement.querySelector('.card__title').textContent = card.name;
-
-  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-
-  return cardElement;
-}
-
-function deleteCard(event) {
-  event.target.closest('.card').remove();
-}
-
-function renderCards() {
-  initialCards.forEach((card) => {
-    const cardElement = createCard(card);
-    cardsList.append(cardElement);
-  });
-}
-
-function getCardTemplate() {
-  return document.querySelector('#card-template').content;
-}
-
-function getCardsList() {
-  return document.querySelector('.places__list');
-}
-const cardsList = getCardsList();
-renderCards();
-
 function createCardElement(card) {
   const cardTemplate = getCardTemplate();
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
