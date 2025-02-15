4b34746ef445d62fbd299c0173c5b7430531cd9c
Roman <roman_polyan@mail.ru>
2025-02-15 13:22:10 +0300

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+const cardsList = getCardsList();
+renderCards();
 
-// @todo: DOM узлы
+function createCardElement(card) {
+  const cardTemplate = getCardTemplate();
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__title').textContent = card.name;
 
-// @todo: Вывести карточки на страницу
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  return cardElement;
+}
+
+function deleteCard(event) {
+  event.target.closest('.card').remove();
+}
+
+function renderCards() {
+  initialCards.forEach((card) => {
+    const cardElement = createCardElement(card);
+    cardsList.append(cardElement);
+  });
+}
+
+function getCardTemplate() {
+  return document.querySelector('#card-template').content;
+}
+
+function getCardsList() {
+  return document.querySelector('.places__list');
+}
